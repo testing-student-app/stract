bcb2aa26b251ebd8fd3c81910795e8e4a7749e6d
Reidond <reidond@gmail.com>
2020-03-23 20:36:07 +0200

    feat(admi_core_tauri): server loading

--- admin_core/src-tauri/src/main.rs
+++ admin_core/src-tauri/src/main.rs
@@ -1,27 +1,45 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod cmd;
 
-fn main() {
-  tauri::AppBuilder::new()
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(_) => {}
-        Ok(command) => {
-          match command {
-            // definitions for your custom commands from Cmd here
-            MyCustomCommand { argument } => {
-              //  your command code
-              println!("{}", argument);
+use std::env;
+use std::sync::{Arc, Mutex};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tauri::AppBuilder::new()
+        .invoke_handler(|_webview, arg| {
+            use cmd::Cmd::*;
+
+            match serde_json::from_str(arg) {
+                Err(_) => {}
+                Ok(command) => match command {
+                    LoadServer { callback, error } => tauri::execute_promise(
+                        _webview,
+                        move || {
+                            let target_exe = env::current_exe()?;
+                            let target_dir = target_exe.parent().unwrap();
+
+                            std::process::Command::new("./go-server")
+                                .arg("-addr")
+                                .arg(":8081")
+                                .current_dir(target_dir)
+                                .spawn()
+                                .expect("failed to execute go-server");
+
+                            Ok("{status: 'started'}".to_string())
+                        },
+                        callback,
+                        error,
+                    ),
+                },
             }
-          }
-        }
-      }
-    })
-    .build()
-    .run();
+        })
+        .build()
+        .run();
+
+    Ok(())
 }
