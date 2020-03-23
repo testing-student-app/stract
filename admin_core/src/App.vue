7bdcdb22c0d4fb402d59ec681b296dbe6f7c9be0
Reidond <reidond@gmail.com>
2020-03-23 20:35:20 +0200

    feat(admi_core): load server on start

--- admin_core/src/App.vue
+++ admin_core/src/App.vue
@@ -2,6 +2,10 @@
   <v-app>
     <v-app-bar app dense>
       <v-toolbar-title>Stract Admin</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title v-if="serverStatus === 'started'"
+        >Not connected, server running</v-toolbar-title
+      >
     </v-app-bar>
 
     <v-content>
@@ -13,8 +17,29 @@
 </template>
 
 <script>
+import tauri from 'tauri/api';
+import { mapActions, mapState } from 'vuex';
+
 export default {
   name: 'App',
+  computed: {
+    ...mapState({
+      serverStatus: state => state.serverStatus,
+    }),
+  },
+  created() {
+    tauri
+      .promisified({
+        cmd: 'loadServer',
+      })
+      .then(({ status }) => {
+        this.setServerStatus(status);
+        this.toggleServerLoaded();
+      });
+  },
+  methods: {
+    ...mapActions(['toggleServerLoaded', 'setServerStatus']),
+  },
 };
 </script>
 
