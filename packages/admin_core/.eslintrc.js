de22beed0a008f87c5112996b97405f7305c5297
Reidond <reidond@gmail.com>
2020-03-29 14:10:25 +0300

    chore(admin_core): disable rule

--- packages/admin_core/.eslintrc.js
+++ packages/admin_core/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    'import/prefer-default-export': 'off',
     'no-shadow': 'off',
     'no-param-reassign': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
