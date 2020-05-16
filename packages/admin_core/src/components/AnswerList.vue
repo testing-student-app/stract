798005ee014d1f252cbe51db2b8f5bfa9092968f
Orest <orest-2@yandex.ua>
2020-05-16 15:08:56 +0300

    refactore answer list

---
+++ packages/admin_core/src/components/AnswerList.vue
@@ -0,0 +1,213 @@
+<template>
+  <b-list-group v-if="item">
+    <b-list-group-item
+      v-for="(answer, i) in item.answers"
+      :key="i"
+      class="answer-item"
+    >
+      <div class="d-flex justify-content-between">
+        <div class="d-flex align-items-center w-75">
+          <b-iconstack
+            font-scale="2"
+            class="mr-2"
+            @click="toggleRight(`${item.id}${i}`, answer)"
+          >
+            <b-icon
+              v-if="getState(`${item.id}${i}`, 'edit')"
+              stacked
+              icon="square"
+            ></b-icon>
+            <b-icon
+              stacked
+              :icon="answer.right ? 'check' : 'x'"
+              :variant="answer.right ? 'success' : 'danger'"
+            ></b-icon>
+          </b-iconstack>
+          <span v-if="!getState(`${item.id}${i}`, 'edit')">
+            {{ `${i + 1}) ${answer.text}` }}
+          </span>
+          <b-form-input
+            v-if="getState(`${item.id}${i}`, 'edit')"
+            v-model="answer.text"
+            placeholder="Enter your answer"
+          ></b-form-input>
+        </div>
+        <div class="d-flex align-items-center justify-content-around w-25">
+          <span
+            v-if="!getState(`${item.id}${i}`)"
+            class="edit-btn px-1"
+            @click="edit(`${item.id}${i}`, i)"
+          >
+            <b-icon icon="pencil"></b-icon>
+            <span class="pl-1">Edit</span>
+          </span>
+          <span
+            v-if="getState(`${item.id}${i}`)"
+            class="ok-btn px-1"
+            @click="ok(`${item.id}${i}`)"
+          >
+            <span class="pl-1">OK</span>
+          </span>
+          <span
+            v-if="!getState(`${item.id}${i}`)"
+            class="del-btn px-1"
+            @click="remove(`${item.id}${i}`, i, item.answers)"
+          >
+            <b-icon icon="trash"></b-icon>
+            <span class="pl-1">Delete</span>
+          </span>
+          <span v-if="getState(`${item.id}${i}`)" class="del-btn px-1">
+            <span class="pl-1" @click="cancel(`${item.id}${i}`)">
+              Cancel
+            </span>
+          </span>
+        </div>
+      </div>
+    </b-list-group-item>
+    <b-list-group-item class="d-flex align-items-center justify-content-center">
+      <b-button
+        squared
+        block
+        variant="light"
+        @click="add(item.id, item.answers)"
+      >
+        <b-icon icon="plus"></b-icon>
+        Add answer
+      </b-button>
+    </b-list-group-item>
+  </b-list-group>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      confirmMode: {},
+    };
+  },
+
+  methods: {
+    getState(id, state = 'confirm') {
+      if (this.confirmMode[id]) {
+        return this.confirmMode[id][state];
+      }
+      return false;
+    },
+    toggleConfirmMode(id, mode = 'confirm') {
+      let newState = {
+        [id]: {
+          [mode]: this.confirmMode[id]
+            ? !this.confirmMode[id][mode]
+            : !this.confirmMode[id],
+          confirm: this.confirmMode[id]
+            ? !this.confirmMode[id].confirm
+            : !this.confirmMode[id],
+        },
+      };
+      if (mode === 'confirm') {
+        newState = {
+          [id]: {
+            [mode]: this.confirmMode[id]
+              ? !this.confirmMode[id][mode]
+              : !this.confirmMode[id],
+          },
+        };
+      }
+      this.confirmMode = {
+        ...this.confirmMode,
+        ...newState,
+      };
+    },
+    toggleRight(id, answer) {
+      if (this.getState(id, 'edit')) {
+        answer.right = !answer.right;
+      }
+    },
+    ok(id) {
+      if (this.getState(id, 'edit')) {
+        this.$confirm('ok', `edit${id}`);
+      } else if (this.getState(id)) {
+        this.$confirm('ok', `remove${id}`);
+      }
+    },
+    cancel(id) {
+      if (this.getState(id, 'edit')) {
+        this.$confirm('cancel', `edit${id}`);
+      } else if (this.getState(id)) {
+        this.$confirm('cancel', `remove${id}`);
+      }
+    },
+    add(id, answers) {
+      answers.push({
+        text: '',
+        right: false,
+      });
+      this.edit(`${id}${answers.length - 1}`, answers.length - 1);
+    },
+    edit(id, i) {
+      this.toggleConfirmMode(id, 'edit');
+      this.$confirm(`edit${id}`)
+        .then(() => {
+          this.toggleConfirmMode(id);
+          console.log(i);
+        })
+        .catch(() => {
+          this.toggleConfirmMode(id);
+        });
+    },
+    remove(id, i, answers) {
+      this.toggleConfirmMode(id);
+      this.$confirm(`remove${id}`)
+        .then(() => {
+          this.toggleConfirmMode(id);
+          answers.splice(i, 1);
+        })
+        .catch(() => {
+          this.toggleConfirmMode(id);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+%btn {
+  cursor: pointer;
+  text-decoration: underline;
+}
+
+.answer-item {
+  &:hover {
+    transform: scale(1.005);
+    margin: 0.5px;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
+  }
+  .ok-btn {
+    &:hover {
+      @extend %btn;
+      color: #28a745;
+      text-decoration: underline;
+    }
+  }
+  .edit-btn {
+    &:hover {
+      @extend %btn;
+      color: #ffc107;
+      text-decoration: underline;
+    }
+  }
+  .del-btn {
+    &:hover {
+      @extend %btn;
+      color: #dc3545;
+    }
+  }
+}
+</style>
--- packages/admin_core/src/components/TestList.vue
+++ packages/admin_core/src/components/TestList.vue
@@ -17,88 +17,8 @@
             </b-button>
           </template>
 
-          <template v-slot:row-details="{ item, index }">
-            <b-list-group>
-              <b-list-group-item
-                v-for="(answer, i) in item.answers"
-                :key="i"
-                class="answer-item"
-              >
-                <div class="d-flex justify-content-between">
-                  <div class="d-flex align-items-center w-75">
-                    <b-iconstack
-                      font-scale="2"
-                      class="mr-2"
-                      @click="toggleRight(`${index}${i}`, answer)"
-                    >
-                      <b-icon
-                        v-if="getState(`${index}${i}`, 'edit')"
-                        stacked
-                        icon="square"
-                      ></b-icon>
-                      <b-icon
-                        stacked
-                        :icon="answer.right ? 'check' : 'x'"
-                        :variant="answer.right ? 'success' : 'danger'"
-                      ></b-icon>
-                    </b-iconstack>
-                    <span v-if="!getState(`${index}${i}`, 'edit')">
-                      {{ `${i + 1}) ${answer.text}` }}
-                    </span>
-                    <b-form-input
-                      v-if="getState(`${index}${i}`, 'edit')"
-                      v-model="answer.text"
-                      placeholder="Enter your answer"
-                    ></b-form-input>
-                  </div>
-                  <div
-                    class="d-flex align-items-center justify-content-around w-25"
-                  >
-                    <span
-                      v-if="!getState(`${index}${i}`)"
-                      class="edit-btn px-1"
-                      @click="edit(`${index}${i}`, i)"
-                    >
-                      <b-icon icon="pencil"></b-icon>
-                      <span class="pl-1">Edit</span>
-                    </span>
-                    <span
-                      v-if="getState(`${index}${i}`)"
-                      class="ok-btn px-1"
-                      @click="ok(`${index}${i}`)"
-                    >
-                      <span class="pl-1">OK</span>
-                    </span>
-                    <span
-                      v-if="!getState(`${index}${i}`)"
-                      class="del-btn px-1"
-                      @click="remove(`${index}${i}`, i, item.answers)"
-                    >
-                      <b-icon icon="trash"></b-icon>
-                      <span class="pl-1">Delete</span>
-                    </span>
-                    <span v-if="getState(`${index}${i}`)" class="del-btn px-1">
-                      <span class="pl-1" @click="cancel(`${index}${i}`)">
-                        Cancel
-                      </span>
-                    </span>
-                  </div>
-                </div>
-              </b-list-group-item>
-              <b-list-group-item
-                class="d-flex align-items-center justify-content-center"
-              >
-                <b-button
-                  squared
-                  block
-                  variant="light"
-                  @click="add(index, item.answers)"
-                >
-                  <b-icon icon="plus"></b-icon>
-                  Add answer
-                </b-button>
-              </b-list-group-item>
-            </b-list-group>
+          <template v-slot:row-details="{ item }">
+            <AnswerList :item="item" />
           </template>
         </b-table>
       </b-col>
@@ -109,8 +29,14 @@
 <script>
 import { mapState } from 'vuex';
 
+import AnswerList from './AnswerList.vue';
+
 export default {
   name: 'TestList',
+
+  components: {
+    AnswerList,
+  },
 
   data() {
     return {
@@ -125,7 +51,6 @@ export default {
         },
         'actions',
       ],
-      confirmMode: {},
     };
   },
 
@@ -134,97 +59,10 @@ export default {
       tests: state => state.tests.testsTomlData.questions,
     }),
   },
-
-  methods: {
-    getState(id, state = 'confirm') {
-      if (this.confirmMode[id]) {
-        return this.confirmMode[id][state];
-      }
-      return false;
-    },
-    toggleConfirmMode(id, mode = 'confirm') {
-      let newState = {
-        [id]: {
-          [mode]: this.confirmMode[id]
-            ? !this.confirmMode[id][mode]
-            : !this.confirmMode[id],
-          confirm: this.confirmMode[id]
-            ? !this.confirmMode[id].confirm
-            : !this.confirmMode[id],
-        },
-      };
-      if (mode === 'confirm') {
-        newState = {
-          [id]: {
-            [mode]: this.confirmMode[id]
-              ? !this.confirmMode[id][mode]
-              : !this.confirmMode[id],
-          },
-        };
-      }
-      this.confirmMode = {
-        ...this.confirmMode,
-        ...newState,
-      };
-    },
-    toggleRight(id, answer) {
-      if (this.getState(id, 'edit')) {
-        answer.right = !answer.right;
-      }
-    },
-    ok(id) {
-      if (this.getState(id, 'edit')) {
-        this.$confirm('ok', `edit${id}`);
-      } else if (this.getState(id)) {
-        this.$confirm('ok', `remove${id}`);
-      }
-    },
-    cancel(id) {
-      if (this.getState(id, 'edit')) {
-        this.$confirm('cancel', `edit${id}`);
-      } else if (this.getState(id)) {
-        this.$confirm('cancel', `remove${id}`);
-      }
-    },
-    add(id, answers) {
-      answers.push({
-        text: '',
-        right: false,
-      });
-      this.edit(`${id}${answers.length - 1}`, answers.length - 1);
-    },
-    edit(id, i) {
-      this.toggleConfirmMode(id, 'edit');
-      this.$confirm(`edit${id}`)
-        .then(() => {
-          this.toggleConfirmMode(id);
-          console.log(i);
-        })
-        .catch(() => {
-          this.toggleConfirmMode(id);
-        });
-    },
-    remove(id, i, answers) {
-      this.toggleConfirmMode(id);
-      this.$confirm(`remove${id}`)
-        .then(() => {
-          this.toggleConfirmMode(id);
-          answers.splice(i, 1);
-        })
-        .catch(() => {
-          this.toggleConfirmMode(id);
-        });
-    },
-  },
 };
 </script>
 
 <style lang="scss" scoped>
-%btn {
-  cursor: pointer;
-  text-decoration: underline;
-}
-
 ::v-deep .tests-table {
   thead {
     th {
@@ -232,33 +70,6 @@ export default {
       top: 0px;
       background: #ffffff;
       z-index: 2;
-    }
-  }
-  .answer-item {
-    &:hover {
-      transform: scale(1.005);
-      margin: 0.5px;
-      box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.5);
-    }
-    .ok-btn {
-      &:hover {
-        @extend %btn;
-        color: #28a745;
-        text-decoration: underline;
-      }
-    }
-    .edit-btn {
-      &:hover {
-        @extend %btn;
-        color: #ffc107;
-        text-decoration: underline;
-      }
-    }
-    .del-btn {
-      &:hover {
-        @extend %btn;
-        color: #dc3545;
-      }
     }
   }
 }
